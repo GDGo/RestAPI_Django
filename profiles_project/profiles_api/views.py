33631b2ff4e118b6e2f4a13b14e6eb6c94e2b8f1
GDGo <50753433+GDGo@users.noreply.github.com>
2025-05-03 18:56:18 +0300

    Добавил HelloApiView и метод get

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View
+    class based view разбивает свою функциональность на методы, которые
+    соответствуют http методам
+    Каждый метод при вызове должен возвращать объект типа Response"""
+
+    def get(self, request, format=None):
+        """request - все заголовки, все типы и прочее,
+        что приходит в запросе от пользователя
+        format - указание на формат ответ json и прочее,
+        если не указано, то используется тип формат, который
+        пришел в запросе"""
+        an_apiview = [
+            ''
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
